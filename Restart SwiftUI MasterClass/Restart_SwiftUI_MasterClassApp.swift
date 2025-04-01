be8ef7567de0c717140537ef2c1ceebfcb3918aa
Aran Fononi <aaronfononi@gmail.com>
2025-04-01 11:21:41 +0330

    Initial Commit

---
+++ Restart SwiftUI MasterClass/Restart_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUI_MasterClassApp.swift
+//  Restart SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
