0fad7f058f40f19de6181ed0b63721b7860f84d8
Aran Fononi <aaronfononi@gmail.com>
2025-04-01 13:46:15 +0330

    Add design elements to Onboarding View

--- Restart SwiftUI MasterClass/Screens/OnboardingView.swift
+++ Restart SwiftUI MasterClass/Screens/OnboardingView.swift
@@ -19,6 +19,90 @@ struct OnboardingView: View {
             Color("ColorBlue")
                 .ignoresSafeArea(.all, edges: .all)
             VStack(spacing: 20) {
+                // MARK: - Header
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                    
+                    Text("""
+It's not how much we give but
+how much love we put into giving.
+""")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } // Header
+                // MARK: - Center
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //ZSack
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } // Center
+                
+                Spacer()
+                // MARK: - Footer
+                
+                // Background
+                ZStack {
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // Call-To-Action
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .offset(x: 20)
+                    
+                    // Capsule
+                    HStack {
+                        Capsule()
+                            .fill(.colorRed)
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    // Circle
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(.colorRed)
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundStyle(.white)
+                        .frame(height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } // HStack
+                    
+                } // Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
                 
             } // VStack
         } // ZStack
