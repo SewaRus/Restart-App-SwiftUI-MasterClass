5c6396cfa2ef9d7982e87a58ceacb80c3fcf70f0
Aran Fononi <aaronfononi@gmail.com>
2025-04-01 11:41:44 +0330

    Create 2 Different Screen and assign them to ContentView

---
+++ Restart SwiftUI MasterClass/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 1/4/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart SwiftUI MasterClass/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Restart SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 1/4/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
